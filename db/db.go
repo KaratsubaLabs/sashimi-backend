ca6eaafbf9c727786d0b8845e73c46c43209e574
kugelblitz2 <48634762+kugelblitz2@users.noreply.github.com>
2022-01-27 10:55:41 -0500

    finish db requests

--- db/db.go
+++ db/db.go
@@ -4,21 +4,20 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"time"
 
 	_ "github.com/lib/pq"
 )
 
-const OutageLogDBName = "downtimelog"
-const SitesDBName = "sitestomonitor"
+func Connect() (*sql.DB, error) {
 
-func Connect(dbname string) (*sql.DB, error) {
 	sourceString := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		os.Getenv("SASHIMI_DB_HOST"),
 		os.Getenv("SASHIMI_DB_PORT"),
 		os.Getenv("SASHIMI_DB_USER"),
 		os.Getenv("SASHIMI_DB_PASSWORD"),
-		dbname,
+		os.Getenv("SASHIMI_DB_NAME"),
 	)
 
 	db, err := sql.Open("postgres", sourceString)
@@ -27,40 +26,110 @@ func Connect(dbname string) (*sql.DB, error) {
 	} else {
 		return db, nil
 	}
+
 }
 
 func LogOutage(db *sql.DB, o Outage) error {
-	command := `SELECT start FROM $1 WHERE service=$2, end=0`
-	_, exists := db.Query(command, OutageLogDBName, o.serviceName)
 
-	if exists == sql.ErrNoRows {
-		command = `INSERT INTO $1 VALUE service=$2, type=$3, start=$4, end=0`
-		_, err := db.Exec(command, OutageLogDBName, o.serviceName, o.outageType, o.outageStart)
+	_, err := db.Query(`SELECT start FROM OutageList WHERE Service=$1, End=0`, o.serviceName)
 
-		if err == nil {
-			return err
-		} else {
-			return nil
-		}
+	if err == sql.ErrNoRows {
+
+		_, _ = db.Exec(`UPDATE ServiceList SET OK=0 WHERE ServiceName=$1`, o.serviceName)
+		_, err = db.Exec(
+			`INSERT INTO OutageList VALUE Service=$1, Start=$2, End=0`, o.serviceName, o.outageStart,
+		)
+
+		return err
+
 	} else {
 		return nil
 	}
+
 }
 
 func LogOK(db *sql.DB, o Outage) error {
-	command := `SELECT start FROM $1 WHERE service=$2, end=0`
-	outageStart, exists := db.Query(command, OutageLogDBName, o.serviceName)
 
-	if exists != sql.ErrNoRows {
-		command = `UPDATE $1 SET end=$2 WHERE start=$3`
-		_, err := db.Exec(command, OutageLogDBName, o.outageEnd, outageStart)
+	_, err := db.Query(`SELECT start FROM OutageList WHERE Service=$1, End=0`, o.serviceName)
 
-		if err == nil {
-			return err
-		} else {
-			return nil
-		}
+	if err != sql.ErrNoRows {
+
+		_, _ = db.Exec(`UPDATE ServiceList SET OK=1 WHERE ServiceName=$1`, o.serviceName)
+		_, err = db.Exec(`UPDATE OutageList SET end=$1 WHERE ServiceName=$2, End=0`, o.outageEnd, o.serviceName)
+		return err
+
 	} else {
 		return nil
 	}
+
+}
+
+func QueryStats(db *sql.DB) (Stats, error) {
+
+	var rows int32
+	db.QueryRow(`SELECT COUNT(Name) FROM ServiceList`).Scan(&rows)
+
+	if rows == 0 {
+		return Stats{}, nil
+	}
+
+	data, err := db.Query(`SELECT Name, OK FROM ServiceList`)
+
+	if err != nil {
+		return Stats{}, err
+	}
+
+	defer data.Close()
+	stats := Stats{
+		serviceName: make([]string, rows),
+		status:      make([]bool, rows),
+	}
+
+	for i := 0; data.Next(); i++ {
+		data.Scan(&stats.serviceName[i], &stats.status[i])
+	}
+
+	return stats, nil
+
+}
+
+func DetailedStats(db *sql.DB, name string) (DetailStats, error) {
+
+	var status bool
+	var since int64
+	if db.QueryRow(`SELECT OK, Since FROM ServiceList WHERE Name=$1`, name).Scan(&status, &since) == sql.ErrNoRows {
+		return DetailStats{}, sql.ErrNoRows
+	}
+
+	var rows int32
+	db.QueryRow(`SELECT COUNT(Name) FROM OutageList WHERE Name=$1`, name).Scan(&rows)
+
+	data, err := db.Query(`SELECT Start, End FROM OutageList`)
+
+	if err != nil {
+		return DetailStats{}, err
+	}
+
+	defer data.Close()
+	detail := DetailStats{
+		status:        status,
+		timeMonitored: time.Now().Unix() - since,
+		downtime:      0,
+		outageStart:   make([]int64, rows),
+		outageEnd:     make([]int64, rows),
+	}
+
+	for i := 0; data.Next(); i++ {
+
+		data.Scan(&detail.outageStart[i], &detail.outageEnd[i])
+
+		if detail.outageEnd[i] != 0 {
+			detail.downtime += detail.outageEnd[i] - detail.outageStart[i]
+		} else {
+			detail.downtime += time.Now().Unix() - detail.outageStart[i]
+		}
+
+	}
+
+	return detail, nil
 }
--- db/models.go
+++ db/models.go
@@ -1,8 +1,27 @@
 package db
 
 type Outage struct {
-	serviceName string
-	outageType  int16
-	outageStart uint64
-	outageEnd   uint64
+	serviceName string `sql:"Service"`
+	outageStart int64  `sql:"Start"`
+	outageEnd   int64  `sql:"End"`
+}
+
+type Service struct {
+	serviceName     string `sql:"Name"`
+	serviceURL      string `sql:"URL"`
+	status          bool   `sql:"OK"`
+	monitoringSince int64  `sql:"Since"`
+}
+
+type Stats struct {
+	serviceName []string
+	status      []bool
+}
+
+type DetailStats struct {
+	status        bool
+	timeMonitored int64
+	downtime      int64
+	outageStart   []int64
+	outageEnd     []int64
 }
