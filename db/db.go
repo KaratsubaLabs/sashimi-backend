1471cfa673989bb1481319b65d2c2be10f927a62
kugelblitz2 <48634762+kugelblitz2@users.noreply.github.com>
2022-01-27 22:32:51 -0500

    add db init functions

--- db/db.go
+++ db/db.go
@@ -29,6 +29,50 @@ func Connect() (*sql.DB, error) {
 
 }
 
+func Migrate(db *sql.DB) error {
+
+	_, err1 := db.Exec(`CREATE TABLE ServiceList()`)
+	_, err2 := db.Exec(`CREATE TABLE OutageList()`)
+
+	if err1 == sql.ErrConnDone || err2 == sql.ErrConnDone {
+		return sql.ErrConnDone
+	}
+
+	db.Exec(`ALTER TABLE ServiceList ADD Name VARCHAR`)
+	db.Exec(`ALTER TABLE ServiceList ADD URL VARCHAR`)
+	db.Exec(`ALTER TABLE ServiceList ADD OK BOOL`)
+	db.Exec(`ALTER TABLE ServiceList ADD Since BIGINT`)
+
+	db.Exec(`ALTER TABLE OutageList ADD Service VARCHAR`)
+	db.Exec(`ALTER TABLE OutageList ADD Start BIGINT`)
+	db.Exec(`ALTER TABLE OutageList ADD End BIGINT`)
+
+	return nil
+
+}
+
+func AddService(db *sql.DB, s Service) error {
+
+	_, err := db.Exec(
+		`INSERT INTO ServiceList VALUE Name=$1, URL=$2, OK=1, Since=$3`, s.serviceName, s.serviceURL, time.Now().Unix(),
+	)
+
+	return err
+
+}
+
+func RemoveService(db *sql.DB, s Service) error {
+
+	_, err := db.Exec(`DELETE FROM ServiceList WHERE Name=$1`, s.serviceName)
+
+	if err != nil {
+		_, err = db.Exec(`DELETE FROM ServiceList WHERE URL=$1`, s.serviceURL)
+	}
+
+	return err
+
+}
+
 func LogOutage(db *sql.DB, o Outage) error {
 
 	_, err := db.Query(`SELECT start FROM OutageList WHERE Service=$1, End=0`, o.serviceName)
