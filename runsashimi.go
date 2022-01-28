e28fb8ad9f25a58dc3b8507005a367125e37362b
kugelblitz2 <48634762+kugelblitz2@users.noreply.github.com>
2022-01-28 07:38:35 -0500

    Add autostart message

--- runsashimi.go
+++ runsashimi.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	server "github.com/karatsubalabs/sashimi-backend/api"
 	"github.com/karatsubalabs/sashimi-backend/sashimi"
 )
 
 func main() {
+
+	fmt.Println("Autostarting Sashimi")
 	go sashimi.Start()
+
 	server.Start()
+
 }
